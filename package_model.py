39376afe429dd0c74aa5b2ca2482de99d6977a2a
Alejandro DGB <alex@adgb.com.au>
2022-05-24 23:22:03 +1000

    Update save command and move learner to build directory

--- package_model.py
+++ package_model.py
@@ -2,14 +2,23 @@
 from fastai.vision.all import *
 from puparazzi import PuparazziService
 
-# Load learner
-learner = load_learner('models/pup-arrazi_resnet101.pkl')
-
 # Create a service instance
 svc = PuparazziService()
+
+# Save path for deployment
+spath = Path(f'{os.environ["HOME"]}/bentoml/repository/{svc.name}/')
+
+# Load learner
+lpath = os.path.dirname(os.path.abspath(__file__))
+learner = load_learner(f'{lpath}/models/pup-arrazi_resnet101.pkl', cpu=True)
+# Update path from the one saved when creating the model
+learner.path = spath
 
 # Pack the trained model artifact
 svc.pack('learner', learner)
 
-# Save the model server to disk for model serving
-saved_path = svc.save()
+# Save to directory
+svc.save()
+
+# Move learner to package version
+shutil.move(f'{spath}/learner.pkl', f'{spath}/{svc.version}/{svc.name}')
