ec8ee07a2a08b144fffa64abcb61af216c916713
Alejandro DGB <alex@adgb.com.au>
2022-05-24 23:22:36 +1000

    Create json response of top 5 predictions

--- puparazzi.py
+++ puparazzi.py
@@ -1,13 +1,16 @@
 # pup-arazzi.py
 
-from bentoml.frameworks.fastai import Fastai1ModelArtifact
+from bentoml.frameworks.fastai import FastaiModelArtifact
 from bentoml.adapters import FileInput
 from fastcore.utils import tuplify, detuplify
+from io import BytesIO
+import json
+import numpy as np
 
 import bentoml
 
 # Define the model artifact for prediction
-@bentoml.artifacts([Fastai1ModelArtifact('learner')])
+@bentoml.artifacts([FastaiModelArtifact('learner')])
 
 # Define the environment. Infer pip package requirements.
 @bentoml.env(infer_pip_packages=True)
@@ -18,14 +21,27 @@ class PuparazziService(bentoml.BentoService):
     # Define the api
     @bentoml.api(input=FileInput(), batch=False)
     def predict(self, file):
-        files = [i.read() for i in files]
+        # Read bytes 
+        file = BytesIO(file.read())
+        
+        # Create dataloader
         dl = self.artifacts.learner.dls.test_dl([file.read()], rm_type_tfms=None, num_workers=0)
+        
+        # Get predictions
         inp, preds, _, dec_preds = self.artifacts.learner.get_preds(dl=dl, with_input=True, with_decoded=True)
-        i = getattr(self.artifacts.learner.dls, 'n_inp', 1)
-        inp = (inp,)
-        dec_list = self.artifacts.learner.dls.decode_batch(inp + tuplify(dec_preds))
-        res = []
-        for dec in dec_list:
-            dec_inp, dec_targ = map(detuplify, [dec[:i],dec[i:]])
-            res.append(dec_targ)
-        return res
+        
+        # Get classes
+        classes = self.artifacts.learner.dls.vocab
+        
+        # Convert predictions to numpy objects
+        predictions = np.array(preds.tolist()).reshape(-1)
+        
+        # Calculate the top 5 predictions, sort them
+        k=5
+        idxs = np.argpartition(predictions, -k)[-k:]
+        probs = np.partition(predictions, -k)[-k:]
+        cls = [classes[i] for i in idxs]
+        topk = sorted([(p,c,i) for p,c,i in zip(probs,cls,idxs)],reverse=True)
+
+        # Return a json object with probabilities, classes, and indexes in the classes list
+        return json.dumps([{'prob':float(p),'class':c,'idx':int(i)} for p,c,i in topk])
